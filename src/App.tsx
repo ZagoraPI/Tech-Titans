06c62b1caa30f32463f2bda17051b8f55ef799d1
PBthb <pbf2fpb@gmail.com>
2025-03-18 22:26:51 +0200

    component-commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,22 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Calendar } from "@/components/ui/calendar"
+import React from "react";
+
 
 function App() {
   const [count, setCount] = useState(0)
-
+  const [date, setDate] = React.useState<Date | undefined>(new Date())
+ 
+  return (
+    <Calendar
+      mode="single"
+      selected={date}
+      onSelect={setDate}
+      className="rounded-md border"
+    />
+  )
   return (
     <>
       <div>
