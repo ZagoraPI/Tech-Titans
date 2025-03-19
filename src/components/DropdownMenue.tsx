aa03a6e1e315950a666f6669eca1b21c2df54748
N1ki2K <pan07nik@gmail.com>
2025-03-19 10:17:24 +0200

    Niki-component

--- src/components/DropdownMenue.tsx
+++ src/components/DropdownMenue.tsx
@@ -2,7 +2,7 @@
 
 import * as React from "react"
 import * as DropdownMenuPrimitive from "@radix-ui/react-dropdown-menu"
-import { Check, ChevronRight, Circle } from "lucide-react"
+import { ChevronRight } from "lucide-react"
 import './DropdownMenue.css';  
 
 const DropdownMenu = DropdownMenuPrimitive.Root
