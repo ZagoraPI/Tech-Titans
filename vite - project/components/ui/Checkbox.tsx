b72ce2b561abafb5355e75955d7b05b4703188a0
H1is4 <hristina.8b.27@gmail.com>
2025-03-20 20:39:16 +0200

    Checkbox  - Hrisa

---
+++ vite - project/components/ui/Checkbox.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+interface CheckboxInputProps {
+  id?: string;
+}
+
+export const CheckboxI: React.FC<CheckboxInputProps> = ({ id }) => {
+  return (
+    <input type="checkbox" id={id} />
+  );
+};
+
+import './checkbox.css';
+import { CheckboxI as Checkbox } from "./Checkbox";
+
+export function CheckboxDemo() {
+  return (
+    <div className="flex items-center space-x-2">
+      <Checkbox id="terms" />
+      <label
+        htmlFor="terms"
+        className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+      >
+        Accept terms and conditions
+      </label>
+    </div>
+  )
+}
--- vite - project/src/App.tsx
+++ vite - project/src/App.tsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { CheckboxI as Checkbox } from "../components/ui/Checkbox"
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <><>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -16,6 +18,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
+      
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
@@ -28,7 +31,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
+    </><Checkbox /></>
   )
 }
 
