7ecdd5383de8574187761a9d6b3060a127664ef7
N1ki2K <pan07nik@gmail.com>
2025-03-24 20:44:08 +0200

    Niki_Component_&_JJ_vladilena_fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,41 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
 import { Alert, AlertTitle, AlertDescription } from "./components/ui/alert";
-import { Avatar, AvatarFallback, AvatarImage } from "./components/VladilenaTest";
+import { Avatar, AvatarImage } from "./components/VladilenaTest";
 import TextBox from "./components/TextBox";
-
+import { ThemeSwitch } from "./components/Niki_Components/ThemeSwitch.tsx";
+import TheJj from "./components/TheJj.tsx";
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>ZaraPI</h1>
-
-      {count > 0 && (
-        <div className="fixed bottom-4 left-1/2 transform -translate-x-1/2 w-full max-w-sm">
-          <Alert>
-            <AlertTitle>Notification</AlertTitle>
-            <AlertDescription>The button has been pressed {count} times</AlertDescription>
-          </Alert>
-        </div>
-      )}
+  <>
+            <div className="justify-center flex flex-row">
+                <ThemeSwitch />
+                <TheJj />
+            </div>
       
-      <Avatar>
-  <AvatarImage src="https://cdn.discordapp.com/attachments/1295438251188031558/1352925497629081702/Screenshot_5.png?ex=67dfc98e&is=67de780e&hm=92a9804b6a4852814f9d276baa08850ae39422961a4a558879dd50e61a698de6&" />
-  <AvatarFallback>CN</AvatarFallback>
-</Avatar>
-    
-      <div className="card">
-
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-
-        <TextBox count1={count} style={{ color: 'white', backgroundColor: 'blue' }} />
-
-      </div>
-
-      <div className="card">
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+            {count > 0 && (
+              <div className="fixed bottom-4 left-1/2 transform -translate-x-1/2 w-full max-w-sm">
+                <Alert>
+                  <AlertTitle>Notification</AlertTitle>
+                  <AlertDescription>The button has been pressed {count} times</AlertDescription>
+                </Alert>
+              </div>
+            )}  
+            
+            <div className="fixed top-4 left-0 w-full max-w-sm">
+            <Avatar>
+              <AvatarImage src="https://files.oaiusercontent.com/file-UisNwbknrK2KrChFkh4yeD?se=2025-03-24T18%3A45%3A57Z&sp=r&sv=2024-08-04&sr=b&rscc=max-age%3D604800%2C%20immutable%2C%20private&rscd=attachment%3B%20filename%3Da6b89eec-1753-439d-a668-72afbd00e0d3.webp&sig=fyQSamTsow1CzY1JNig7tgiyRKy3CGgx9owIp3tZbbA%3D"  />
+            </Avatar>
+            </div>
+  
+            <div className="card">
+              <button onClick={() => setCount((count) => count + 1)}>
+                count is {count}
+              </button>
+              <TextBox count1={count} style={{ color: 'white', backgroundColor: 'blue' }} />
+            </div>
     </>
   );
 }
