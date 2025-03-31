021f53cf871251a8ffb875f67a2fe5bf186363b9
GitPresly <presly56782@gmail.com>
2025-03-31 16:52:14 +0300

    PreslyFix1

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import AlexGifShower from "./components/Alex_Components/AlexGifShower.tsx";
 import AlexPfpIcon from "./components/Alex_Components/AlexPfpIcon.tsx";
 import { Bozhilkata } from "./components/BozhilkataComp.tsx"
 import { Button } from "@/components/ui/button"
-import {Accordion, AccordionContent, AccordionItem, AccordionTrigger, } from "@/components/preslyComp"
+import {Accordion, AccordionContent, AccordionItem, AccordionTrigger, } from "@/components/Presly_Components/Accordion.tsx"
 
 
 function App() {
@@ -60,12 +60,12 @@ function App() {
               </button>
               <TextBox count1={count} style={{ color: 'white', backgroundColor: 'blue' }} />
             </div>
-            
+
             <Accordion type="single" collapsible>
   <AccordionItem value="item-1">
-    <AccordionTrigger>Is it accessible?</AccordionTrigger>
+    <AccordionTrigger>Is it accessible????</AccordionTrigger>
     <AccordionContent>
-      Yes. It adheres to the WAI-ARIA design pattern.
+      Maybe idk :3
     </AccordionContent>
   </AccordionItem>
 </Accordion>
