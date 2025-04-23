7af3acbc44073bae9fe8a75e4ba36278e674eb96
GitPresly <presly56782@gmail.com>
2025-04-23 20:23:39 +0300

    Testing Regex

--- src/components/Presly_Components/Presly-Form.tsx
+++ src/components/Presly_Components/Presly-Form.tsx
@@ -27,7 +27,7 @@ export function PreslyForm() {
   const [emailError, setEmailError] = useState("");
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   const [kilogramError, setKilogramError] = useState("");
-  const kilogramRegex = /^[20-420]+$/;
+  const kilogramRegex = /^[1-9]+$/;
 
 
   const isFormValid = () => {
