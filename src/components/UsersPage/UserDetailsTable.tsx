cd8de1eb109f8493f90ce883381c07c4b710dbb6
Bozhilkata <pbf2fpb@gmail.com>
2025-05-17 22:25:17 +0300

    Petar users list (UsersDetails update) (#96)
    * Added UsersPage folder adn updated the imports
    
    * style: changed the style of the background
    
    * feat: moved the refresh button in the searchBar comp and added a button to home directory
    
    * style: removed and changed the style of the details and background
    
    * fix: removed comments
    
    * style: changed the sytel of the users list
    
    * styel: changed color of the refresh button
    
    * style: changed position of the details
    
    * feat: updated by adding information adn style to the list
    
    * style: table  margine 2 to 1 rem
    
    * fix: removed whitespaced adn Insert semicolon for consistency
    
    * fix: added the missin in formation about the user
    
    * feat: added every information for the deatils of the users

--- src/components/UsersPage/UserDetailsTable.tsx
+++ src/components/UsersPage/UserDetailsTable.tsx
@@ -14,10 +14,10 @@ const UserDetails: React.FC<UserDetailsProps> = ({ user, onClose }) => (
     style={{
       position: 'absolute',
       top: '50%',
-      left: '60%',
+      left: '0%',
       transform: 'translate(0, -50%)',
       height: 'auto',
-      width: '400px',
+      width: '600px',
       background: '#0000',
       boxShadow: '-4px 0 10px rgba(247, 10, 10, 0.2)',
       padding: '2rem',
@@ -25,28 +25,79 @@ const UserDetails: React.FC<UserDetailsProps> = ({ user, onClose }) => (
       animation: 'slideRight 0.3s ease forwards',
     }}
   >
-    <h2 style={{ marginBottom: '1rem' }}>{user.name}</h2>
-<table style={{ width: '100%', marginBottom: '1.5rem' }}>
+    <h2 style={{ marginBottom: '20px' }}> {user.id}---{user.name}---{user.id}</h2>
+<table style={{ width: '100%'}}>
   <tbody>
     <tr>
-      <td style={{ ...td, color: 'black' }}>Name:</td> {/* black */}
-      <td style={td}>{user.name}</td>
+      <td style={{ ...td, color: 'white', textAlign: 'left', width: '120px', verticalAlign: 'top' }}>Address:</td>
+      <td style={{ ...td, paddingLeft: '0.1rem', textAlign: 'left' }}>
+        <div style={{ display: 'flex', flexDirection: 'column', gap: '0.2rem' }}>
+          <div>
+            <strong>Street:</strong>
+            <span style={{ marginLeft: '7px' }}>{user.address?.street}</span>
+          </div>
+          <div>
+            <strong>City:</strong>
+            <span style={{ marginLeft: '7px' }}>{user.address?.city}</span>
+          </div>
+          <div>
+            <strong>Zipcode:</strong>
+            <span style={{ marginLeft: '7px' }}>{user.address?.zipcode}</span>
+          </div>
+          <div>
+            <strong>Suite:</strong>
+            <span style={{ marginLeft: '7px' }}>{user.address?.suite}</span>
+          </div>
+        </div>
+      </td>
     </tr>
     <tr>
-      <td style={{ ...td, color: 'white' }}>ID:</td> {/* white */}
-      <td style={td}>{user.id}</td>
+      <td style={{ ...td, color: 'white', textAlign: 'left', width: '120px', verticalAlign: 'top' }}>Geo:</td>
+      <td style={{ ...td, paddingLeft: '0.1rem', textAlign: 'left' }}>
+        <div style={{ display: 'flex', flexDirection: 'column', gap: '0.2rem' }}>
+          <div>
+            <strong>Lat:</strong>
+            <span style={{ marginLeft: '7px' }}>{user.geo?.lat}</span>
+          </div>
+          <div>
+            <strong>Lng:</strong>
+            <span style={{ marginLeft: '7px' }}>{user.geo?.lng}</span>
+          </div>
+        </div>
+      </td>
     </tr>
     <tr>
-      <td style={{ ...td, color: 'white' }}>Email:</td> {/* white */}
-      <td style={td}>{user.email}</td>
+      <td style={{ ...td, color: 'white', textAlign: 'left', width: '120px', verticalAlign: 'top' }}>Phone:</td>
+      <td style={{ ...td, paddingLeft: '0.1rem', textAlign: 'left' }}>
+        <strong>Phone:</strong>
+        <span style={{ marginLeft: '7px' }}>{user.phone}</span>
+      </td>
     </tr>
     <tr>
-      <td style={{ ...td, color: 'white' }}>Phone:</td> {/* white */}
-      <td style={td}>{user.phone}</td>
+      <td style={{ ...td, color: 'white', textAlign: 'left', width: '120px', verticalAlign: 'top' }}>Website:</td>
+      <td style={{ ...td, paddingLeft: '0.1rem', textAlign: 'left' }}>
+        <strong>Website:</strong>
+        <span style={{ marginLeft: '7px' }}>{user.website}</span>
+      </td>
     </tr>
     <tr>
-      <td style={{ ...td, color: 'white' }}>Address:</td> {/* white */}
-      <td style={td}>{user.address?.street}, {user.address?.city}</td>
+      <td style={{ ...td, color: 'white', textAlign: 'left', width: '120px', verticalAlign: 'top' }}>Company:</td>
+      <td style={{ ...td, paddingLeft: '0.1rem', textAlign: 'left' }}>
+        <div style={{ display: 'flex', flexDirection: 'column', gap: '0.2rem' }}>
+          <div>
+            <strong>Name:</strong>
+            <span style={{ marginLeft: '7px' }}>{user.company?.name}</span>
+          </div>
+          <div>
+            <strong>CatchPhrase:</strong>
+            <span style={{ marginLeft: '7px' }}>{user.company?.catchPhrase}</span>
+          </div>
+          <div>
+            <strong>BS:</strong>
+            <span style={{ marginLeft: '7px' }}>{user.company?.bs}</span>
+          </div>
+        </div>
+      </td>
     </tr>
   </tbody>
 </table>
--- src/models/model.ts
+++ src/models/model.ts
@@ -1,15 +1,25 @@
 export interface User {
-    company: any;
     id: number;
     name: string;
     username: string;
     email: string;
-    phone: string;
     address: {
       street: string;
       suite: string;
       city: string;
       zipcode: string;
     };
+    geo: {
+      lat: string;
+      lng: string;
+    };
+    phone: string;
+    website: string;
+    company: {
+      name: string; 
+      catchPhrase: string;
+      bs: string; 
+    };
+
   }
   
